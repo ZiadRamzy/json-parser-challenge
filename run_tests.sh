76dd62c5bd80b8efa61bce66c3401a8c6ba4bba1
Ziad Abdelfatah <ge64cot@mytum.de>
2024-11-01 01:47:22 +0100

    Updated run_tests.sh to include tests for new valid and invalid JSON files in tests/step2.

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+# step 1 tests
+echo "Testing step 1 JSON files:"
 python parser.py tests/step1/valid.json
 echo "Exit code: $?"
 
 python parser.py tests/step1/invalid.json
+echo "Exit code: $?"
+
+
+# step 2 tests
+echo "Testing step 2 JSON files:"
+python parser.py tests/step2/valid.json
+echo "Exit code: $?"
+
+python parser.py tests/step2/valid2.json
+echo "Exit code: $?"
+
+python parser.py tests/step2/invalid.json
+echo "Exit code: $?"
+
+python parser.py tests/step2/invalid2.json
 echo "Exit code: $?"
